e34bd7ecfefed5d5d9c294199d5cf4e0aad987fd
rv castro <423004999@ntc.edu.ph>
2026-09-11 19:15:50 +0800

    Added CabreraProfile

---
+++ app/src/test/java/quarter2/CabreraProfile.java
@@ -0,0 +1,21 @@
+package quarter2;
+
+import org.junit.Test;
+
+public class CabreraProfile {
+    @Test
+    public void printMyProfile() {
+        // --- 1. THE INPUT (storing your personal details in variables) ---
+        String myName = "Scarlet";
+        String myFavFood = "Menudo";
+        String myPetName = "Cyrus";
+        int myAge = 16;
+
+        // ---2. THE OUTPUT (printing to the console)---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is" + myName + "and i am " + myAge );
+        System.out.println( "My pets name is " + myPetName);
+
+
+    }
+}
