61816378a07df90be1f14b1f0e2bd9948576bdee
Denise <dimasangaldenise7@gmail.com>
2026-09-11 14:56:16 +0800

    Initial commit

---
+++ app/src/test/java/quarter2/AcujedoProfile.java
@@ -0,0 +1,20 @@
+package quarter2;
+
+import org.junit.Test;
+
+public class AcujedoProfile {
+    @Test
+    public void printMyProfile() {
+        // --- 1. THE INPUT (storing your personal details in variables) ---
+        String myName = "khurt";
+        String myFavFood = "sinigang";
+        String petName = "Kaizo";
+        int myAge = 16;
+
+        // ---2. THE OUTPUT (printing to the console)---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is" + myName + "and i am " + myAge );
+
+
+    }
+}
