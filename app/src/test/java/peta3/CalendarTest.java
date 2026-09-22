c895c4c1114860429edcf56edfa7399a0a9a0d86
11-SIS- Lao Clyde Austin G. <muda94109@gmail.com>
2026-09-22 13:29:37 +0800

    e

--- app/src/test/java/peta3/CalendarTest.java
+++ app/src/test/java/peta3/CalendarTest.java
@@ -12,7 +12,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class CalendarTest {
+public class qCalendarTest {
 
     static Scanner scanner = new Scanner(System.in);
 
