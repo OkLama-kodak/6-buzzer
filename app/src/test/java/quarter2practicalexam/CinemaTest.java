91289ecdeddc4d0c5aec74567103f477d1631752
ARKIN CARL <arkincarlalonzo0130@gmail.com>
2026-09-23 15:00:26 +0800

    commited CinemaTest : to be continued

---
+++ app/src/test/java/quarter2practicalexam/CinemaTest.java
@@ -0,0 +1,19 @@
+package quarter2practicalexam;
+
+import org.junit.Test;
+
+public class CinemaTest {
+    @Test
+    public static void main(String[] args) {
+        int choice;
+
+        do {
+            System.out.println("==========================");
+            System.out.println("      Cinema       ");
+            System.out.println("1. Buy tickets");
+            System.out.println("2. Buy Snacks");
+            System.out.println("3. Exit");
+
+        }
+    }
+}
