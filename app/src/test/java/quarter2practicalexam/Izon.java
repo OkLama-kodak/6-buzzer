08e45ac7ac066af2bc3f3bb246e743ec188f4ffc
Ellah245 <ellahamante3@gmail.com>
2026-09-23 15:03:41 +0800

    Added Izon something

--- app/src/test/java/quarter2practicalexam/Izon.java
+++ app/src/test/java/quarter2practicalexam/Izon.java
@@ -1,4 +1,18 @@
 package quarter2practicalexam;
+
+    import org.junit.Test;
+    import java.io.ByteArrayInputStream;
+    import java.util.Scanner;
 public class Izon {
-    System.out.println("Hi sir")
+
+    public void testArcadeCounterTest(){
+       StringBuilder automatedInput = new StringBuilder();
+
+       System.out.println("----Generating Arcade Test Data----");
+
+       automatedInput.append("1\n");//Buy tokens
+
+        automatedInput.append("2\n");//Choose Claim Prize
+    }
+
 }
