d1003789cedf0d2050442b26bd4d8e8457c3e6c2
ARKIN CARL <arkincarlalonzo0130@gmail.com>
2026-09-25 14:51:18 +0800

    commited FastFoodTest.java : renamed the work

--- app/src/test/java/quarter2practicalexam/FastFoodTest.java
+++ app/src/test/java/quarter2practicalexam/FastFoodTest.java
@@ -2,15 +2,15 @@ package quarter2practicalexam;
 
 import org.junit.Test;
 
-public class CinemaTest {
+public class FastFoodTest {
     @Test
     public static void main(String[] args) {
         int choice;
 
         do {
             System.out.println("==========================");
-            System.out.println("      Cinema       ");
-            System.out.println("1. Buy tickets");
+            System.out.println("      Khurt's FastFood       ");
+            System.out.println("1. ");
             System.out.println("2. Buy Snacks");
             System.out.println("3. Exit");
 
