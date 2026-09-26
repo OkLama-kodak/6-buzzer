297ba4e7d477205b2887dd324e41c1358eb6c97b
OkLama <adobongsapatos9@gmail.com>
2026-09-26 22:08:05 +0800

    Added main idea will add simulation later

--- app/src/test/java/quarter2practicalexam/Izon.java
+++ app/src/test/java/quarter2practicalexam/Izon.java
@@ -1,7 +1,14 @@
 package quarter2practicalexam;
 
+import java.io.IOException;
 
 public class Izon {
     public static void main(String[] args) {
-}
-}
+        boolean running = true;
+        int choice = 0;
+        //Simulated Inputs
+        String[] simulatedInputs = {
+                //Step 1 and 2 buy tokens and low ticket
+                "1", //Step 1: Buying tokens
+                "2", //Step 2: Choosing the price
+                "200",//Step 3: Low ticket count
