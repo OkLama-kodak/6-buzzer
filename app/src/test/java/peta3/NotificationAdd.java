e8db86b1ec9baba80c37e83291052957d09f5d9e
OkLama <adobongsapatos9@gmail.com>
2026-09-26 20:39:57 +0800

    Added skeleton

--- app/src/test/java/peta3/NotificationAdd.java
+++ app/src/test/java/peta3/NotificationAdd.java
@@ -22,7 +22,8 @@ public static void main(String[] args) {
         System.out.println("==========================");
         System.out.println("1. Add Notification");
         System.out.println("2. View Notification");
-        System.out.println("3. Edit Notification");
+        System.out.println("3. Edit" +
+                " Notification");
         System.out.println("4. Exit");
         System.out.println("==========================");
 
--- app/src/test/java/quarter2practicalexam/Izon.java
+++ app/src/test/java/quarter2practicalexam/Izon.java
@@ -1,18 +1,9 @@
 package quarter2practicalexam;
 
-    import org.junit.Test;
-    import java.io.ByteArrayInputStream;
-    import java.util.Scanner;
+
 public class Izon {
+    public static void main(String[] args) {
 
-    public void testArcadeCounterTest(){
-       StringBuilder automatedInput = new StringBuilder();
 
-       System.out.println("----Generating Arcade Test Data----");
-
-       automatedInput.append("1\n");//Buy tokens
-
-        automatedInput.append("2\n");//Choose Claim Prize
-    }
-
+}
 }
